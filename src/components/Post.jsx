edb3e0af2dfd28fd4fca8f27ff189c928b3ea2e5
hakkhant <hakhant27@gmail.com>
2022-07-05 19:38:36 +0630

    updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,17 +10,17 @@ const Post = ({ post }) => {
         className='object-cover object-center w-full h-64'
         src={post.image}
       />
-      <header className='flex items-center justify-between px-3 py-1 leading-tight md:px-3 md:py-1'>
+      <header className='flex items-center justify-between px-2 py-2 leading-tight md:px-2 md:py-2'>
         <h1 className='text-md'>
           <span className='text-black'>{post.title}</span>
         </h1>
       </header>
 
-      <div className='px-3 py-1 leading-tight md:px-3 md:py-1'>
+      <div className='px-2 py-2 leading-tight md:px-2 md:py-2'>
         <p className='text-sm text-grey-500'>{post.content}</p>
       </div>
 
-      <footer className='flex items-center justify-between px-3 py-1 leading-none md:px-3 md:py-1'>
+      <footer className='flex items-center justify-between px-2 py-2 leading-none md:px-2 md:py-2'>
         <small className='text-xs text-gray-500 '>{user}</small>
       </footer>
     </article>
