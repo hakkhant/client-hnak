528e705e7b47a6718d34fc202824672564e1b499
hakkhant <hakhant27@gmail.com>
2022-07-05 18:59:59 +0630

    routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import PostForm from './pages/PostForm';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
 const App = () => {
   return (
     <Router>
       <div className='max-w-6xl mx-auto px-4 lg:px-0'>
         <Header />
-        <Routes>
-          <Route path='/' element={<Home />}>
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/create' element={<PostForm />} />
-          </Route>
-        </Routes>
         <Footer />
       </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import PostForm from './pages/PostForm';
+
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/create' element={<PostForm />} />
+      </Route>
+    </Routes>
   </React.StrictMode>
 );
