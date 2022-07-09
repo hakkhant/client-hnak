d9d96204fc823ccb953e9945170a87e3859b7535
Htet Aung Khant <104135037+hakkhant@users.noreply.github.com>
2022-07-09 20:08:53 +0630

    Update App.js
    removed register link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+iimport { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import Register from './pages/Register';
 import PostForm from './pages/PostForm';
 import Layout from './components/Layout';
 
@@ -12,7 +11,6 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='login' element={<Login />} />
-          {/* <Route path='register' element={<Register />} /> */}
           <Route path='create' element={<PostForm />} />
         </Route>
       </Routes>
